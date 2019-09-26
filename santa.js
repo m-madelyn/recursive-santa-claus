96b6fa911a2af95d1f00f16e047ec0fb372c7bee
m-madelyn <m.madelyn@live.com>
2019-09-25 21:01:45 -0400

    fixes function for delivering presents

--- santa.js
+++ santa.js
@@ -1,5 +1,13 @@
+
 function deliverPresents(houses) {
+  if(houses.length === 0) return 0
   console.log('Delivering presents to ' + houses[0])
+
+  var first = houses.slice[0]
+  var rest = houses.slice(1, houses.length)
+  
+  deliverPresents(rest)
 }
 
 module.exports = deliverPresents
+
